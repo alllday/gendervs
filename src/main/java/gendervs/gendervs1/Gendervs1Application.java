b352fcb8be02718bb1df68dd60563ef1d731eb11
allday <whddlf2345@gmail.com>
2025-08-16 17:14:35 +0900

    init: spring boot project start

---
+++ src/main/java/gendervs/gendervs1/Gendervs1Application.java
@@ -0,0 +1,13 @@
+package gendervs.gendervs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gendervs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gendervs1Application.class, args);
+	}
+
+}
